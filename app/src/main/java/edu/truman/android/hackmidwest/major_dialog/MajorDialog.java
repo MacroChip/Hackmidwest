169c61813f033548d7b70db1bbb0472b3cb6979d
Viktor Yankoff <viktoryankoff@gmail.com>
2014-07-19 21:47:06 -0500

    blah

--- app/src/main/java/edu/truman/android/hackmidwest/major_dialog/MajorDialog.java
+++ app/src/main/java/edu/truman/android/hackmidwest/major_dialog/MajorDialog.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.bluetooth.BluetoothClass;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
@@ -35,35 +36,30 @@ public class MajorDialog extends RoboDialogFragment {
     @Inject
     ExperienceBank experienceBank;
 
-    ArrayList<ExperienceEntry> majorsList = new ArrayList<ExperienceEntry>();
-    HashMap<String, ArrayList<Company>> majorToCompanyMap = new HashMap<String, ArrayList<Company>>();
-
-    List<String> majors = new ArrayList<String>();
+    Map<String, Integer> mapIdToKey;
+    Map<Integer, List<ExperienceEntry>> majorToCompanyMap = new HashMap<Integer, List<ExperienceEntry>>();
 
     @Override
     public void onCreate(Bundle b) {
         super.onCreate(b);
 //        populateMajors(majorsBank.getMajors(), experienceBank.getCompanies())
-            populateMajors(majors);
+//            populateMajors(majors);
     }
 
     @Override
     public Dialog onCreateDialog(Bundle bundle) {
+        mapMajorsToKeys();
+        populateMajors(majorsBank.getMajors(), experienceBank.getCompanies());
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View convertView = (View) inflater.inflate(R.layout.majors_list_view, null);
         ListView lv = (ListView) convertView.findViewById(R.id.lv);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, majors);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, majorsBank.getMajors());
         lv.setAdapter(adapter);
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String major = (String) parent.getItemAtPosition(position);
-
-                ArrayList<Company> companies = majorToCompanyMap.get(major);
-//                Company company = majorToCompanyMap.get(major).get(0);
-                Toast.makeText(getActivity(), companies.size(), Toast.LENGTH_SHORT).show();
-//                Intent i = new Intent(MajorDialog.this, CompanyListActivity.class);
-//                i.putExtra(CompanyListFragment.LIST, company);
+                Log.d("BOOOM", mapIdToKey.get(major) + "");
             }
         });
 
@@ -74,46 +70,27 @@ public class MajorDialog extends RoboDialogFragment {
                 .create();
     }
 
-    private void populateMajors(List<String> majors) {
-        majors.add("computer science");
-        ArrayList<Company> csCompanies = new ArrayList<Company>();
-        csCompanies.add(new Company("CERNER", 15));
-        csCompanies.add(new Company("ASYNCHRONY", 25));
-        csCompanies.add(new Company("BOEING", 35));
-        majorToCompanyMap.put("computer science", csCompanies);
 
-        majors.add("accounting");
-        ArrayList<Company> accCompanies = new ArrayList<Company>();
-        accCompanies.add(new Company("EARNST AND YOUNG", 5));
-        accCompanies.add(new Company("KPMG", 15));
-        accCompanies.add(new Company("EDWARD JONES", 25));
-        majorToCompanyMap.put("accounting", accCompanies);
+    private void mapMajorsToKeys() {
+        List<String> majors = majorsBank.getMajors();
+        Map<Integer, String> mapIdToKey = new HashMap<Integer, String>();
+        int i = 0;
+        for (String major : majors) {
+            mapIdToKey.put(i, major);
+            i++;
+        }
 
-
-        majors.add("biology");
-        ArrayList<Company> bioCompanies = new ArrayList<Company>();
-        accCompanies.add(new Company("MONSANTO", 155));
-        accCompanies.add(new Company("ALDI", 135));
-        accCompanies.add(new Company("LOGAN UNIVERSITY", 245));
-        majorToCompanyMap.put("accounting", bioCompanies);
-
-        majors.add("business");
-        majors.add("chemistry");
-        majors.add("physics");
-        majors.add("mathematics");
     }
 
-//
-//    private void populateMajors(List<String> majors, List<ExperienceEntry> experience) {
-//        majorsList.clear();
-//        for (String major : majors) {
-//            for (ExperienceEntry entry : experience) {
-//                if (entry.getMajors().contains(major)) {
-//                    majorsList.add(entry);
-//                }
-//            }
-//            majorToCompanyMap.put(major, majorsList);
-//            majorsList.clear();
-//        }
-//    }
+    private void populateMajors(List<String> majors, List<ExperienceEntry> experience) {
+        for (String major : majors) {
+            List<ExperienceEntry> experienceCompanyList = new ArrayList<ExperienceEntry>();
+            for (ExperienceEntry entry : experience) {
+                if (entry.getMajors().contains(major)) {
+                    experienceCompanyList.add(entry);
+                }
+            }
+            majorToCompanyMap.put(mapIdToKey.get(major), experienceCompanyList);
+        }
+    }
 }
